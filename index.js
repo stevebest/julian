020171b1230198c24195f4d4e9adc2db33d5f421
Dominic Tarr <dominic.tarr@gmail.com>
2017-05-27 15:47:03 +1200

    reimplement in a way to avoid having any floating point errors

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 module.exports = convert;
 module.exports.toDate = convertToDate;
 
+module.exports.toJulianDay = toJulianDay;
+module.exports.toMillisecondsInJulianDay = toMillisecondsInJulianDay;
+module.exports.fromJulianDayAndMilliseconds = fromJulianDayAndMilliseconds;
+
+var DAY = 86400000;
+var HALF_DAY = DAY/2;
+var UNIX_EPOCH_JULIAN_DATE = 2440587.5;
+var UNIX_EPOCH_JULIAN_DAY = 2440587;
+
 function convert(date) {
-  return (((+date) / 86400000) + 2440587.5).toFixed(6);
+  return (toJulianDay(date) + (toMillisecondsInJulianDay(date)/DAY)).toFixed(6);
 };
 
 function convertToDate(julian) {
-  return new Date((Number(julian) - 2440587.5) * 86400000);
+  return new Date((Number(julian) - UNIX_EPOCH_JULIAN_DATE) * DAY);
+};
+
+function toJulianDay (date) {
+  return ~~((+date+HALF_DAY)/DAY) + UNIX_EPOCH_JULIAN_DAY;
+};
+
+function toMillisecondsInJulianDay (date) {
+  return (+date+HALF_DAY)%DAY;
+};
+
+function fromJulianDayAndMilliseconds(day, ms) {
+  return (day-UNIX_EPOCH_JULIAN_DATE)*DAY + ms;
 };
