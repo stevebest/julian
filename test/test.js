cbfeb7978a33b1e26449fc0b09b175eb6f396d35
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2017-07-10 22:31:40 +0700

    Semicolons and stuff :)

--- test/test.js
+++ test/test.js
@@ -15,7 +15,6 @@ test('from JD to Date', function (t) {
   t.equal(+julian.toDate('2440587.500000'), +new Date(0), 'unix epoch');
   t.equal(+julian.toDate('2451545.000000'), +new Date('2000-01-01T12:00:00.000Z'), 'noon of Jan 1, 2000');
 
-
   t.end();
 });
 
@@ -31,62 +30,44 @@ test('convert back and forth without floating point errors', function (t) {
     'now is converted to julian and back safely'
   );
 
-
   t.equal(
     julian.toMillisecondsInJulianDay(new Date('2000-01-01T12:00:00.000Z')),
     0,
     'the day starts at noon, so noon is zero milliseconds'
-  )
+  );
 
   t.equal(
     julian.toJulianDay(new Date('1970-01-01T00:00:00.000Z')),
     2440587,
     'unix epoch starts on julian day 2440587'
-  )
+  );
 
   t.equal(
     julian.toMillisecondsInJulianDay(new Date('1970-01-01T00:00:00.000Z')),
     43200000,
     'unix epoch starts with half a julian day'
-  )
+  );
 
   t.equal(
     julian.toJulianDay(new Date('1970-01-01T12:00:00.000Z')),
     2440588,
     'first noon in unix epoch starts julian day 2440588'
-  )
+  );
 
   t.equal(
     julian.toMillisecondsInJulianDay(new Date('1970-01-01T12:00:00.000Z')),
     0,
     'unix epoch starts with half a julian day'
-  )
-
+  );
 
   //the instance in time known as j2000, aka terriestial time
   t.equal(
     julian.toJulianDay(new Date('2000-01-01T12:00:00.000Z')),
     2451545,
     'toJulianDay is give correct julian day for j2000 epoch'
-  )
+  );
   // test data taken from https://en.wikipedia.org/wiki/Epoch_(astronomy)#Julian_years_and_J2000
-
-  console.log(
-    new Date(julian.fromJulianDayAndMilliseconds(0, 0))
-  )
 
   t.end();
 
-})
-
-
-
-
-
-
-
-
-
-
-
-
+});
